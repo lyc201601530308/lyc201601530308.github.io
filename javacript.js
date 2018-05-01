ccb71ccda4d20264975913da1ba26fd6e7888619
lyc201601530308 <38377667+lyc201601530308@users.noreply.github.com>
2018-05-01 23:16:33 +0800

    Update javacript.js

--- javacript.js
+++ javacript.js
@@ -1,6 +1,8 @@
-function showpic(whichpic){
+function showPic(whichpic){
   var source=whichpic.getAttribute("href");
-  var placeholder=document.getElementById("placeholder");
+  var placeholder = document.getElementById("placeholder");
   placeholder.setAttribute("src",source);
+  var text = whichpic.getAttribute("title");
+  var description = document.getElementById("description");
+  description.firstChild.nodeValue = text;
 }
-  
